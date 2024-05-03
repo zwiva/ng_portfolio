6af63d9ce2976501c1ed12d6cc7ea8b9d170bc50
Andrea Castro <ap.castrocastellon@gmail.com>
2024-05-03 01:21:10 -0400

    scaffoling basic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        title: 'Home',
+        loadComponent: () => import('./features/home/components/view/view.component').then(mod => mod.ViewComponent)
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+];
