ec78aeaba2d8a144ca17ea672e542d8a868f7bf8
Andrea Castro <ap.castrocastellon@gmail.com>
2024-05-16 01:01:42 -0400

    version 2: simplified shades, order colors & scss files, add playground section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
         loadComponent: () => import('./features/home/components/view/view.component').then(mod => mod.ViewComponent)
     },
     {
+        path: 'playground',
+        title: 'playground',
+        loadComponent: () => import('./features/playground/components/view/view.component').then(mod => mod.ViewComponent)
+    },
+    {
         path: 'error-page',
         title: 'error',
         loadComponent: () => import('./features/error_page/components/view/view.component').then(mod => mod.ViewComponent)
