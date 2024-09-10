a905bca24d6862a6481c625e82f9d35d6ca1fac6
Andrea Castro <ap.castrocastellon@gmail.com>
2024-09-10 02:22:18 -0300

    agrega librerias xlsx y save-file, crea xlsx y descarga

--- src/app/features/xlsx_feature/components/view/view.component.ts
+++ src/app/features/xlsx_feature/components/view/view.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../../../shared/components/footer/footer.component';
+import * as XLSX from 'xlsx';
+import { read, utils, writeFile } from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-view',
@@ -11,22 +14,22 @@ import { FooterComponent } from '../../../../shared/components/footer/footer.com
 })
 export class ViewComponent {
 
-  nuevo: any = {
-    "id": 11,
+  nuevo: any = { // desde servicio
     "orden": "OT1",
-    "usuario": "root10",
-    "fecha_creacion": "2024-08-23",
-    "fecha_recibido": "2024-08-23",
-    "cantidad": 10,
-    "anotaciones": "transferencia 1 test",
     "estado": "Transferido",
+    "fecha_recibido": "2024-08-23",
+    "fecha_creacion": "2024-08-23",
+    "usuario": "root10",
     "almacen_origen": "Almacén 1",
     "origen_direccion": "General Jofre 100",
     "almacen_destino": "Almacén 2",
     "destino_direccion": "Marin 014",
+    "anotaciones": "transferencia 1 test",
     "id_estado": 2,
     "id_origen": 1,
     "id_destino": 2,
+    "cantidad": 10,
+    "id": 11,
     "productos": [
       {
         "cantidad": 5,
@@ -44,16 +47,70 @@ export class ViewComponent {
   }
 
   crearxlsx() {
-    console.log('nuevo', this.nuevo);
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([]);
+    const verticalDataA = [
+      'Código', //orden
+      'Estado', //estado
+      'Fecha transferencia', //fecha_transferencia 
+      'Fecha creación', //fecha_creacion
+      'Ordenado por', //usuario
+      'Tienda origen', //almacen_origen
+      'Dirección origen', //origen_direccion
+      'Tienda destino', //almacen_destino
+      'Dirección destino', //destino_direccion
+      'Anotaciones', //anotaciones
+      '', // espacio a proposito 
+      'Articulos' // titulo tabla
+    ];
+    const verticalDataB = [
+      this.nuevo.orden,
+      this.nuevo.estado,
+      this.nuevo.fecha_recibido,
+      this.nuevo.fecha_creacion,
+      this.nuevo.usuario,
+      this.nuevo.almacen_origen,
+      this.nuevo.origen_direccion,
+      this.nuevo.almacen_destino,
+      this.nuevo.destino_direccion,
+      this.nuevo.anotaciones
+    ];
+    verticalDataA.forEach((value, index) => {
+      ws[`A${index + 1}`] = { t: 's', v: value };
+    });
+    verticalDataB.forEach((value, index) => {
+      ws[`B${index + 1}`] = { t: 's', v: value };
+    });
 
+    const tableDataArticles = [
+      ['id', 'nombre', 'identificacion', 'cantidad'], // Encabezados
+    ];
+
+    this.nuevo.productos.forEach((element: any) => {
+      console.log('element', element);
+      let product = [element.id, element.nombre, element.identificacion, element.cantidad];
+      tableDataArticles.push(product)
+    });
+
+    console.log('table productos final', tableDataArticles);
+
+    XLSX.utils.sheet_add_aoa(ws, tableDataArticles, { origin: 'A13' }); // inicio de tabla de articulos
+
+    // Crear el libro de trabajo
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, `OT_id_${this.nuevo.id}`); // nombre hoja excel
+
+    // Generar el archivo Excel
+    const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+
+    // Guardar el archivo
+    this.saveAsExcelFile(excelBuffer, 'OrdenTransferencia'); // nombre archivo
   }
 
-
-
-
-
-
-
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    saveAs(data, `${fileName}_id_${this.nuevo.id}.xlsx`);
+  }
 
 
 }
