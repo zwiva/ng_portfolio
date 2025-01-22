4e94e88ba3cd040003ce02f084a8afe1b024530d
Andrea Castro <ap.castrocastellon@gmail.com>
2025-01-22 20:53:38 -0300

    modify home, start: add projects and samples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,44 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'home',
-        title: 'Home',
-        loadComponent: () => import('./features/home/components/view/view.component').then(mod => mod.ViewComponent)
-    },
-    {
-        path: 'playground',
-        title: 'playground',
-        loadComponent: () => import('./features/playground/components/view/view.component').then(mod => mod.ViewComponent)
-    },
-    {
-        path: 'xlsx_feature',
-        title: 'xlsx',
-        loadComponent: () => import('./features/xlsx_feature/components/view/view.component').then(mod => mod.ViewComponent)
-    },
-    {
-        path: 'error-page',
-        title: 'error',
-        loadComponent: () => import('./features/error_page/components/view/view.component').then(mod => mod.ViewComponent)
-    },
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'home'
-    },
-    {
-        path: '**',
-        pathMatch: 'full',
-        redirectTo: 'error-page'
-    },
+  {
+    path: 'home',
+    title: 'Home',
+    loadComponent: () => import('./features/home/components/view/view.component').then(mod => mod.ViewComponent)
+  },
+  {
+    path: 'projects',
+    title: 'projects',
+    loadComponent: () => import('./features/projects/components/view/view.component').then(mod => mod.ViewComponent)
+  },
+  {
+    path: 'samples',
+    title: 'samples',
+    loadComponent: () => import('./features/samples/components/view/view.component').then(mod => mod.ViewComponent)
+  },
+  {
+    path: 'playground',
+    title: 'playground',
+    loadComponent: () => import('./features/playground/components/view/view.component').then(mod => mod.ViewComponent)
+  },
+  {
+    path: 'xlsx_feature',
+    title: 'xlsx',
+    loadComponent: () => import('./features/xlsx_feature/components/view/view.component').then(mod => mod.ViewComponent)
+  },
+  {
+    path: 'error-page',
+    title: 'error',
+    loadComponent: () => import('./features/error_page/components/view/view.component').then(mod => mod.ViewComponent)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'error-page'
+  },
 ];
---
+++ src/app/features/projects/components/view/view.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../../../../shared/components/footer/footer.component';
+
+@Component({
+  selector: 'app-view',
+  standalone: true,
+  imports: [NavbarComponent, FooterComponent],
+  templateUrl: './view.component.html',
+  styleUrl: './view.component.scss'
+})
+export class ViewComponent {
+
+}
